38f42007c8db88da09212089bef6bd10e0d6ba9a
Firefly <yeliu802@gmail.com>
2024-05-13 16:38:23 -0400

    init

--- app/Http/Controllers/Install/Database.php
+++ app/Http/Controllers/Install/Database.php
@@ -17,6 +17,7 @@ class Database extends Controller
     {
         return view('install.database.create', [
             'host'      => env('DB_HOST'    , 'localhost'),
+            'port'      => env('DB_PORT'    , '3306'),
             'username'  => env('DB_USERNAME', ''),
             'password'  => env('DB_PASSWORD', ''),
             'database'  => env('DB_DATABASE', ''),
